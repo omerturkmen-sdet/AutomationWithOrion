7bc79a5b53d59e70ac84c581d053543ea9090ad2
Ömer Türkmen <102856067+omerturkmen-sdet@users.noreply.github.com>
2023-04-09 03:22:43 +0300

    Update TwitterAccountInfo.java

--- src/test/java/automationPractices/TwitterAccountInfo.java
+++ src/test/java/automationPractices/TwitterAccountInfo.java
@@ -3,7 +3,7 @@ package automationPractices;
 public class TwitterAccountInfo {
 
     /**
-     * Twitter login için gerekli bilgileri burda tutup test kısmında burdan çekiyoruz.
+     * We keep the necessary information for Twitter login here and pull it from here in the test part.
      */
     public static String email = "enteryouremail";
     public static String password = "enteryourpassword";
